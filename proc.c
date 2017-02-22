2c256f9123bd480a91761adc6c99d5eb5712e226
Киндер Анастасия Олеговна <ri67s05@mwp08.csc.local>
2017-02-22 08:58:34 +0700

    proc3

--- proc.c
+++ proc.c
@@ -7,5 +7,56 @@ int day,vklad,doxod,a,b,c;
     scanf("%d",&vklad);
     printf("Введите срок вашего вклада (не более 365 дней): ");
     scanf("%d",&day);
-
+	if (vklad>=10000 && day<=365)
+    {
+        if (vklad<100000) {
+            if (day<=30)
+            {
+                a = vklad*0.1;
+                doxod = vklad-a;
+            } else if (day>30 && day<=120)
+            {
+                a = vklad*0.02;
+                doxod=vklad+a;
+             } else {
+                    if (day>120 && day<=240)
+                    {
+                        a=vklad*0.06;
+                        doxod=vklad+a;
+                    } else {
+                         if (day>240 && day<=365)
+                         {
+                            a=vklad*0.12;
+                            doxod=vklad+a;
+                         }
+                    }
+              }
+        } else {
+            if (day<=30)
+            {
+                a = vklad*0.1;
+                doxod = vklad-a;
+            } else {
+                if (day>30 && day<=120)
+                {
+                    a = vklad*0.03;
+                    doxod=vklad+a;
+                } else {
+                    if (day>120 && day<=240)
+                    {
+                        a=vklad*0.08;
+                        doxod=vklad+a;
+                    } else {
+                         if (day>240 && day<=365)
+                         {
+                            a=vklad*0.15;
+                            doxod=vklad+a;
+                         }
+                    }
+                }
+            }
+        }
+    }
+    printf ("Ваш доход будет составлять: %d", doxod);
+                            
 }
