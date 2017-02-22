8968dfaf24f27363bdc740bac67b843b08ae089c
Киндер Анастасия Олеговна <ri67s05@mwp08.csc.local>
2017-02-22 08:49:36 +0700

    proc2

--- proc.c
+++ proc.c
@@ -2,5 +2,10 @@
 
 int main()
 {
+int day,vklad,doxod,a,b,c;
+    printf("Введите размер вашего вклада(в рублях): ");
+    scanf("%d",&vklad);
+    printf("Введите срок вашего вклада (не более 365 дней): ");
+    scanf("%d",&day);
 
 }
